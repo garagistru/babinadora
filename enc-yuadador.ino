3a0e085252279bf110d868c0ffbf422dc955e841
Mechanic <garagistru@gmail.com>
2025-02-28 15:01:59 +0300

    скорость и длинна набор и сохранение

--- enc-yuadador.ino
+++ enc-yuadador.ino
@@ -9,24 +9,48 @@ const int TickB = 3;  // пин счетчик B
 #include <EEPROM.h>
 int address = 10;
 long lento;
+//-------------------------
+ int speedAddress= 250;
+long spider;
 
 #define TicksA 3    // пин энкодера TickA
 #define TicksB 10    // пин энкодера
 #define ENC_TYPE 1 // тип энкодера, 0 или 1
+//----------------------------------------
+#define TickSA 2    // пин энкодера TickA
+#define TickSB 9    // пин энкодера
+//#define ENC_TYPE 1 // тип энкодера, 0 или 1
+//------------------
 
 volatile boolean states0, lastStates, turnsFlag;
 volatile int encsCounter;
+//------------------------------
+volatile boolean stateS0, lastStateS, turnSFlag;
+volatile int encSCounter;
+
 
 int Ubutton = 11;
 bool flagUbutton = false;
 uint32_t btnTimer = 0;  /// на месте
+//--------------------------------
+int UbuttonS = 8;
+bool flagUbuttonS = false;
+uint32_t btnTimerS = 0;  /// на месте////
+
 void setup()
 {
   Serial.begin(9600);
   attachInterrupt(1, ints0, CHANGE);
   pinMode(Ubutton, INPUT_PULLUP);
   encsCounter = EEPROM.get(address, lento);
+  //-------------------
+  attachInterrupt(0, intS0, CHANGE);
+  pinMode(UbuttonS, INPUT_PULLUP);
+  encSCounter = EEPROM.get(speedAddress, spider);
+
 }
+
+
 
 void ints0()
 {
@@ -43,6 +67,24 @@ void ints0()
     lastStates = states0;
   }
 }
+//------------------------------------------------------
+void intS0()
+{
+  stateS0 = digitalRead(TickSA);
+  if (stateS0 != lastStateS)
+  {
+#if (ENC_TYPE == 1)
+    turnSFlag = !turnSFlag;
+    if (turnSFlag)
+      encSCounter += (digitalRead(TickSB) != lastStateS) ? -1 : 1;
+#else
+    encSCounter += (digitalRead(TickSB) != lastStateS) ? -1 : 1;
+#endif
+    lastStateS = stateS0;
+  }
+}
+
+
 void asignar()
 {
 encsCounter = constrain(encsCounter, 0, 600);
@@ -50,7 +92,7 @@ encsCounter = constrain(encsCounter, 0, 600);
   // coco= constrain(coco,0,600);
   //   Serial.println("coco - "+String(coco));
 
-  Serial.println("encsCounter = " + String(encsCounter));
+  Serial.println("encsCounter-Largo = " + String(encsCounter));
   delay(200);
   int beta=(EEPROM.get(address, lento));
   Serial.println(String(beta));
@@ -78,7 +120,48 @@ encsCounter = constrain(encsCounter, 0, 600);
     // Serial.println("release");
   }
 }
+//---------------
+void asignarSpeed()
+{
+encSCounter = constrain(encSCounter, 1000, 2400);
+  // int coco=map(encCounter,-300,300,0, 600);  ///длинна
+  // coco= constrain(coco,0,600);
+  //   Serial.println("coco - "+String(coco));
+
+  Serial.println(" sPEED encSCounter = " + String(encSCounter));
+  delay(200);
+  int betaS=(EEPROM.get(speedAddress, spider));
+  Serial.println(String(betaS));
+  delay(200);
+  Serial.println("*EEPROM = " + String(EEPROM.get(speedAddress, spider)));
+  delay(600);
+
+  bool btnSState = !digitalRead(UbuttonS);
+  if (btnSState && !flagUbuttonS && millis() - btnTimerS > 100)
+  {
+    flagUbuttonS = true;
+    btnTimerS = millis();
+    // Serial.println("press");
+  }
+  if (btnSState && flagUbuttonS && millis() - btnTimerS > 500)
+  {
+    btnTimerS = millis();
+    EEPROM.put(speedAddress, encSCounter);
+    Serial.println("press sPEED");
+  }
+  if (!btnSState && flagUbuttonS && millis() - btnTimerS > 500)
+  {
+    flagUbuttonS = false;
+    btnTimerS = millis();
+     Serial.println("release");
+  }
+}
+//---------------
+
+
 void loop()
 {
-  asignar( );
+  //asignar( );
+  delay(400);
+  asignarSpeed();
 }
